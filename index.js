1b4c4069ebbf5edad15cf293327892c2ce9d19cb
Ali El Husseini <ali.elhusseini@ctsjounieh.com>
2021-11-25 15:53:56 +0200

    testing

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const app = express()
 mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`server listening to ${PORT}`)))
     .catch(err => console.log('connection error'))
+
 app.use(express.json())
 app.use(cors())
 
